1d6ac0c00a1c6558af7be50bba0dfbac3260e8a6
Fabricray256 <raymondzian@gmail.com>
2019-01-30 19:06:15 +0300

    this is the fizzbuzz challenge

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+numbers = [5, 6, 7, 8, 9]
+list1 = [1,2,3,4]
+
+
+def fizzbuzz(numbers, list1):
+
+    lists = numbers + list1
+    if len(lists) % 3 == 0 and len(lists) % 5 == 0:
+        return("fizzbuzz")
+    elif len(lists) % 3 == 0:
+        return "fizz"
+    elif len(lists) % 5 == 0:
+        return "buzz"
+    else:
+        return (len(lists))
+    print(fizzbuzz(['5', '6', '7', '8', '9'], ['1', '2', '3', '4']))
---
+++ fizzbuzztest.py
@@ -0,0 +1,20 @@
+numbers = [5, 6, 7, 8, 9]
+list1 = [1,2,3,4]
+
+
+def fizzbuzz(numbers, list1):
+
+    lists = list_1 + list_2 
+    if len(lists) % 3 == 0 and len(lists) % 5 == 0:
+        return("fizzbuzz")
+     elif len(lists) % 3 == 0:
+        return "fizz"
+     elif len(lists) % 5 == 0:
+        return "buzz"
+
+    return (len(lists))print(fizzbuzz(['1', '2', '3', '4', '5'], ['1', '2', '3', '9']))
+
+
+
+
+
