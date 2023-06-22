c281bf5fdf537ae280c9955435576e997ee82e6e
Zziwa Raymond Ian <raymondzian@gmail.com>
2023-06-22 21:46:14 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ yob = int(input("Enter your year of birth?"))
 current_year = 2019
 age = current_year - yob         
 if age < 0:
-    print("You donot exist")
+    print("You donot exist")   
 elif < 18:
     print("You are a minor")
 elif 18 <= age < 37:
