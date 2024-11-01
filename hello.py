bab9fef828f9a759fd30026a33b269a4787badec
Zziwa Raymond Ian <raymondzian@gmail.com>
2024-11-01 23:47:40 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ current_year = 2019
 age = current_year - yob         
 if age < 0:
     print("You donot exist")   
-elif < 18:
+elif < 18: 
     print("You are a minor")
 elif 18 <= age < 37:
     print(" you are a youth")    
